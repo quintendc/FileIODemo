f38d877e561d93314a8de49dcf8d4aa286d6db23
Quinten De Clerck <quinten.de.clerck@outlook.com>
2021-10-28 13:33:17 +0200

    [UPD] create file and check if file already exists

--- WpfCreateFileDemo/WpfCreateFileDemo/MainWindow.xaml.cs
+++ WpfCreateFileDemo/WpfCreateFileDemo/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,11 +8,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MessageBox = System.Windows.MessageBox;
 
 namespace WpfCreateFileDemo
 {
@@ -25,14 +28,35 @@ namespace WpfCreateFileDemo
             InitializeComponent();
         }
 
+        string path = "";
+
         private void btnSelectPath_Click(object sender, RoutedEventArgs e)
         {
+            var dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
 
+            path = dialog.SelectedPath;
+            lblPath.Content = path;
         }
 
         private void btnCreateFile_Click(object sender, RoutedEventArgs e)
         {
-
+            if (!string.IsNullOrEmpty(path))
+            {
+                if (!File.Exists(path + $@"\\{txtFileName.Text}{txtFileExtension.Text}"))
+                {
+                    string file = path + "\\" + txtFileName.Text + txtFileExtension.Text;
+                    FileStream stream = new FileStream(file, FileMode.Create);
+                }
+                else
+                {
+                    MessageBox.Show("File already exists!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("No path selected!");
+            }
         }
     }
 }
